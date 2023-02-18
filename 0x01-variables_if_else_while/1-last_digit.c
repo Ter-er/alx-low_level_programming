791f9ba40912300a09c941a03619566c97b2e4e0
Ter-er <tfgbem@gmail.com>
2023-02-17 19:16:40 -0800

    alphabet fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * main - Entry point
- *   * Description: checks on the last digit of the assigned var
- *    * Return: Always 0 (success)
- *     */
+ * main - Entry point
+ * Description: checks on the last digit of the assigned var
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n, m;
