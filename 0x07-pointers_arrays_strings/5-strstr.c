0fff3ad6e8bad4ac54cf0ae433bf0c9c03f0dd93
Ter-er <tfgbem@gmail.com>
2023-03-07 09:09:05 -0800

    Task 5 fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,10 @@
-es (31 sloc)  539 Bytes
-
 #include "main.h"
 
 /**
  * _strstr - locates a substring
  *@haystack: the longer string to search
  * @needle: the first occurrence of the substring
- * Return: a pointer beg of substring or @Null if it not foound.
+ * Return: a pointer beg of substring or @Null if it not found
  */
 char *_strstr(char *haystack, char *needle)
 {
