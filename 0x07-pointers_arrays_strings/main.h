2fcb69c5a353399cbd8c6acb7494a7b4d37648dc
Ter-er <tfgbem@gmail.com>
2023-03-17 05:21:06 -0700

    Task 6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,12 +2,15 @@
 #define MAIN_H
 
 
+
 #include <stdio.h>
 #include <stdlib.h>
 
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
+
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
+void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
@@ -16,5 +19,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-
-#endif 
+#endif
